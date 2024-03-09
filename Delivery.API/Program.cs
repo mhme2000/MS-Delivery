28956058df7ea130187b89b12586b5e6874468d2
Marcos Eckart <Marcos Eckart@ECKART>
2024-03-08 22:22:54 -0300

    Incluindo um consumer do rabbitmq para a fila de pedido pronto

--- Delivery.API/Program.cs
+++ Delivery.API/Program.cs
@@ -1,29 +1,40 @@
 using Delivery.Application.Interfaces.Customers;
 using Delivery.Application.UseCases.Customers;
+using Delivery.Consumer;
 using Delivery.Domain.Interfaces;
 using Delivery.Infrastructure.Contexts;
 using Delivery.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddScoped<IDeliveryRepository, DeliveryRepository>();
-builder.Services.AddScoped<ISendEmailUseCase, SendEmailUseCase>();
-builder.Services.AddDbContext<DeliveryContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-var app = builder.Build();
-using (var scope = app.Services.CreateScope())
+public class Program
 {
-    var dataContext = scope.ServiceProvider.GetRequiredService<DeliveryContext>();
-    dataContext.Database.Migrate();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddScoped<IDeliveryRepository, DeliveryRepository>();
+        builder.Services.AddScoped<ISendEmailUseCase, SendEmailUseCase>();
+        builder.Services.AddDbContext<DeliveryContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        var app = builder.Build();
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        using var scope = app.Services.CreateScope();
+        var dataContext = scope.ServiceProvider.GetRequiredService<DeliveryContext>();
+        dataContext.Database.Migrate();
+        var serviceEmail = scope.ServiceProvider.GetService<ISendEmailUseCase>();
+        if(serviceEmail != null)
+        {
+            Thread threadConsumer = new(() => RabbitConsumer.Consume(serviceEmail));
+            threadConsumer.Start();
+        }
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
 }
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
 
---
+++ Delivery.Consumer/RabbitConsumer.cs
@@ -0,0 +1,50 @@
+﻿using Delivery.Application.Interfaces.Customers;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace Delivery.Consumer;
+public static class RabbitConsumer
+{
+    public record ConsumerModel
+    {
+        public string Email { get; set; }
+    }
+    public static void Consume(ISendEmailUseCase sendEmailUseCase)
+    {
+        ConnectionFactory factory = new()
+        {
+            UserName = "guest",
+            Password = "guest",
+            VirtualHost = "/",
+            HostName = "localhost",
+            Port = 5672
+        };
+
+        IConnection conn = factory.CreateConnection();
+
+        IModel channel = conn.CreateModel();
+        channel.ExchangeDeclare(exchange: "exchange_default_1", type: ExchangeType.Direct, durable: false, autoDelete: false, null);
+        channel.QueueDeclare(queue: "queue_pedido_pronto", durable: true, exclusive: false, autoDelete: false, null);
+        channel.QueueBind(queue: "queue_pedido_pronto", exchange: "exchange_default_1", routingKey: "key_default", null);
+
+        var consumer = new EventingBasicConsumer(channel);        
+        consumer.Received += (ch, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var messageSerialize = Encoding.UTF8.GetString(body);
+            var message = JsonSerializer.Deserialize<ConsumerModel>(messageSerialize);
+            if(!string.IsNullOrEmpty(message.Email))
+                sendEmailUseCase.Execute(new Domain.DTOs.SendEmailDTO { Email = message.Email, Content = "Seu pedido está pronto.", Subject = "Pedido pronto" });
+            Console.WriteLine(" Recebendo a seguinte mensagem: {0}", message);
+            channel.BasicAck(ea.DeliveryTag, false);
+            Thread.Sleep(1000);
+
+        };
+        string consumerTag = channel.BasicConsume(queue: "queue_pedido_pronto", autoAck: false, consumer: consumer);
+        Console.WriteLine("Iniciando consumer");
+    }
+}
+
+
