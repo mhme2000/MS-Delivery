3b71240315dab796ffc8bdca0517fee3ec134fd8
Marcos Eckart <Marcos Eckart@ECKART>
2024-03-16 11:31:15 -0300

    alterando http client para variável não estática

--- Delivery.Infrastructure/Repositories/CustomerRepository.cs
+++ Delivery.Infrastructure/Repositories/CustomerRepository.cs
@@ -3,7 +3,7 @@ using System.Net.Http.Json;
 
 namespace Delivery.Infrastructure.Repositories;
 
-public class CustomerRepository() : ICustomerRepository
+public class CustomerRepository : ICustomerRepository
 {
     private record SearchResponse
     {
@@ -13,11 +13,11 @@ public class CustomerRepository() : ICustomerRepository
         public string? Phone { get; set; }
         public string? Address { get; set; }
     }
-    private static readonly HttpClient _httpClient = new HttpClient();
     public async Task<string> SearchCustomerByCustomerIdAsync(Guid customerId)
     {
         try
         {
+            HttpClient _httpClient = new HttpClient();
             var response = await _httpClient.GetAsync($"http://http-alb-631019594.us-east-1.elb.amazonaws.com/customers/{customerId}");
             response.EnsureSuccessStatusCode();
             var content = await response.Content.ReadFromJsonAsync<SearchResponse>();
